42d2afaae565e9ee35398d28e39b6907bbd39106
murarii07 <abhayaj07@gmail.com>
2024-08-08 01:08:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import Form from './Components/Form';
 import './form.css';
 import RespondedMessage from './Components/Responded';
 import Tables from './Components/Table';  // Ensure correct import
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import NotFound from './Components/notFound';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Form />}></Route>
-          <Route path='/details' element={<Tables />}></Route>
-          <Route path='/submit' element={<RespondedMessage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <Switch>
+
+          <Route   path='/' Component={Form}></Route>
+          <Route path='/details' Component={Tables}></Route>
+          <Route path='/submit' Component={RespondedMessage}></Route>
+          <Route component={NotFound} /> 
+      </Switch>
+    </BrowserRouter>
+    </div >
   );
 }
 
